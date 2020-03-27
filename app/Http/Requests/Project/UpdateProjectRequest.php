ddde75414c87dd201fe670c8850dc8d6b1c5e5c2
davejablo <davejab97@gmail.com>
2020-03-27 16:51:11 +0100

    Project update request name unique fix.

--- app/Http/Requests/Project/UpdateProjectRequest.php
+++ app/Http/Requests/Project/UpdateProjectRequest.php
@@ -24,7 +24,7 @@ class UpdateProjectRequest extends FormRequest
     public function rules()
     {
         return [
-            'name' => 'nullable|string|unique:projects|max:50',
+            'name' => 'nullable|string|max:50',
             'budget' => 'required|numeric|between:0,999999.99',
         ];
     }
